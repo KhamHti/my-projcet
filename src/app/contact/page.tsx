5b3f5f764f008251e8db750cb70f38201dee550f
Khun Kyaw Kham Hti <khamhti@agga.io>
2024-04-02 22:42:00 +0630

    motion transition effect

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,17 @@
-const ContactPage = () => {
-    return <div>ContactPage</div>;
-  };
+"use client";
 
-  export default ContactPage;
-  
+import { motion } from 'framer-motion';
+
+const ContactPage = () => {
+  return (
+    <motion.div className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      Contact
+    </motion.div>
+  )
+};
+
+export default ContactPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,57 @@
+'use client';
+
 import Image from "next/image";
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
-    <div
-      className="flex flex-col lg:flex-row h-full px-4 sm:px-8 
-      md:px-12 lg:px-20 xl:px-48"
+    <motion.div className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
     >
-      {/* image container */}
-      <div className="h-1/2 lg:w-1/2 lg:h-full relative">
-        <Image
-          src={"/hero.png"}
-          alt="hero"
-          fill
-          className="object-contain"
-        />
-      </div>
-      {/* text container   */}
+
       <div
-        className="h-1/2 lg:w-1/2 lg:h-full flex flex-col 
-        items-center justify-center gap-8"
+        className="flex flex-col lg:flex-row h-full px-4 sm:px-8 
+      md:px-12 lg:px-20 xl:px-48"
       >
-        <h1 className="text-4xl font-bold md:text-6xl">
-          Crafting Digital Experiences, Designing Tomorrow.
-        </h1>
-        <p className="md:text-xl">
-          Welcome to my digital canvas, where innovation and creativity
-          converge. With a keen eye for aesthetics and a mastery of code, my
-          portfolio showcases a diverse collection of projects that reflect my
-          commitment to excellence.
-        </p>
-        {/* buttons */}
-        <div className="flex gap-4 w-full">
-          <button
-            className="p-4 rounded-xl ring-1 ring-black 
+        {/* image container */}
+        <div className="h-1/2 lg:w-1/2 lg:h-full relative">
+          <Image
+            src={"/hero.png"}
+            alt="hero"
+            fill
+            className="object-contain"
+          />
+        </div>
+        {/* text container   */}
+        <div
+          className="h-1/2 lg:w-1/2 lg:h-full flex flex-col 
+        items-center justify-center gap-8"
+        >
+          <h1 className="text-4xl font-bold md:text-6xl">
+            Crafting Digital Experiences, Designing Tomorrow.
+          </h1>
+          <p className="md:text-xl">
+            Welcome to my digital canvas, where innovation and creativity
+            converge. With a keen eye for aesthetics and a mastery of code, my
+            portfolio showcases a diverse collection of projects that reflect my
+            commitment to excellence.
+          </p>
+          {/* buttons */}
+          <div className="flex gap-4 w-full">
+            <button
+              className="p-4 rounded-xl ring-1 ring-black 
             hover:cursor-pointer bg-black text-white"
-          >
-            View My Work
-          </button>
-          <button className="p-4 rounded-xl ring-1 ring-black">
-            Contact Me
-          </button>
+            >
+              View My Work
+            </button>
+            <button className="p-4 rounded-xl ring-1 ring-black">
+              Contact Me
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
