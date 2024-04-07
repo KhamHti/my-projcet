bcd21470161839fc500e87d2493038862a9571e3
Khun Kyaw Kham Hti <khamhti@agga.io>
2024-04-08 00:35:10 +0630

    init contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,17 +1,48 @@
 "use client";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+
+const text = "Say Hello";
 
 const ContactPage = () => {
   return (
-    <motion.div className="h-full"
+    <motion.div
+      className="h-full"
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      Contact
+      <div
+        className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20
+        xl:px-48"
+      >
+        {/* text container  */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 2.5,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+                key={index}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            🙂
+          </div>
+        </div>
+        {/* foam container  */}
+        <div className="h-1/2 lg:h-full lg:w-1/2">
+          <span>Dear Kham Hti ,</span>
+        </div>
+      </div>
     </motion.div>
-  )
+  );
 };
 
 export default ContactPage;
