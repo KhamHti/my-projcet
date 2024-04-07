ba0f422307498a1becd5813ad8471b60425ac617
Khun Kyaw Kham Hti <khamhti@agga.io>
2024-04-08 00:14:29 +0630

    portfolio
    sample works

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "khamhti.ggwp.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
